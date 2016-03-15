b98a76aed3e240ee958522389ebbbd868b3805fc
Laura <lauracleave29@gmail.com>
2016-03-15 20:38:51 +1100

    Commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+//DELAYING THE CODE FROM EXECUTING UNTIL ALL DOM ASSETS HAVE BEEN LOADED
+
 $(document).ready(function () {
+
+    //CHANGING THE OPACITY OF .navLogo ON HOVER
+
+    //FADING THE UNDERLINE OF header ul li a ON HOVER
+
+    //SMOOTH SCROLLING TO INTRODUCTION ON CLICK .arrowDown
+
+    //SMOOTH SCROLLING TO TOP ON CLICK .arrowUp
+
+    //'BOUNCE'/'POINT' .rightArrow AND .leftArrow ARROWS ON HOVER
+
+    //'BOUNCE'/'POINT' .rightArrow AND .leftArrow ARROWS ON HOVER
 
 });
